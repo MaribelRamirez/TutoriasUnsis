7fef1c23909d6dc63728c1364a79bd70dbbddd87
Marifer <rsmaribel93>
2018-08-13 21:51:55 -0500

    creación de la clase profesores en el paquete model

---
+++ src/java/model/profesores.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Marifer
+ */
+public class profesores {
+ private int  idProfesor;
+private String nombre;
+private String apellidoP;
+private String apellidoM;
+private int licenciatura;
+
+   public profesores(int idProfesor, String nombre, String apellidoP, String apellidoM) {
+      this.idProfesor = idProfesor;
+      this.nombre = nombre;
+      this.apellidoP = apellidoP;
+      this.apellidoM = apellidoM;
+   }
+
+   public int getIdProfesor() {
+      return idProfesor;
+   }
+
+   public void setIdProfesor(int idProfesor) {
+      this.idProfesor = idProfesor;
+   }
+
+   public String getNombre() {
+      return nombre;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+
+   public String getApellidoP() {
+      return apellidoP;
+   }
+
+   public void setApellidoP(String apellidoP) {
+      this.apellidoP = apellidoP;
+   }
+
+   public String getApellidoM() {
+      return apellidoM;
+   }
+
+   public void setApellidoM(String apellidoM) {
+      this.apellidoM = apellidoM;
+   }
+
+   public int getLicenciatura() {
+      return licenciatura;
+   }
+
+   public void setLicenciatura(int licenciatura) {
+      this.licenciatura = licenciatura;
+   }
+}
